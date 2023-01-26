e083baa177f0e1ab15d7c53738ff6f881674029b
RonexLemon <ronexondimu@gmail.com>
2023-01-26 21:05:43 +0300

    create bider and owner contracts

---
+++ backend/contracts/Bider.sol
@@ -0,0 +1,5 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./TenderOwner.sol";
---
+++ backend/contracts/TenderOwner.sol
@@ -0,0 +1,4 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
