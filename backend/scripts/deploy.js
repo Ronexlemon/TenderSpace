5a6f8f3d8b3e4e7d7d4a1107b06f1cb1201c6b50
RonexLemon <ronexondimu@gmail.com>
2023-01-26 22:03:59 +0300

    redeploy contract

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,5 +1,6 @@
 const {ethers} = require("hardhat")
 ////TenderSpaceAddress 0x82395FC034cd820930445d6A5Ff04D0849bE2a94
+//2. TenderSpaceAddress 0x3bfe526bD2631E8d9417abfe2D1Abc0548B03Be4
 async function main(){
 //get the contract
 const TenderSpaceContract = await ethers.getContractFactory("Bider");
